7aff6e3686755e590505962194a993a15c3ca8ae
Brian <65667828+Briansunyang@users.noreply.github.com>
2022-03-06 21:53:24 +0800

    Update Dualsub.js

--- Dualsub.js
+++ Dualsub.js
@@ -109,9 +109,9 @@ let default_settings = {
     },
     Netflix: {
         type: "Google", // Google, DeepL, External, Disable
-        lang: "English",
+        lang: "Chinese",
         sl: "auto",
-        tl: "en",
+        tl: "ch",
         line: "s", // f, s
         dkey: "null", // DeepL API key
         s_subtitles_url: "null",
